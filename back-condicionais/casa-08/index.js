783fe3f2f6b44a156daf879e87b239c8d1753731
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 11:33:30 -0300

    Lógica da patologia, estudande, maior idadee   - implementadas e testadas - ok

--- back-condicionais/casa-08/index.js
+++ back-condicionais/casa-08/index.js
@@ -1,11 +1,19 @@
-const idade = 18;
+const idade = 70;
 const possuiPatologia = false;
 const altura = 150;
-const ehEstudante = false;
+const ehEstudante = true;
 
 
 if(idade < 12 || idade > 65){
     console.log("ACESSO NEGADO")
 } else if(altura < 150){
     console.log("ACESSO NEGADO")
+} else if(possuiPatologia){
+    console.log("ACESSO NEGADO")
+} else if(ehEstudante){
+    console.log("10 REAIS")
+} else if(idade < 18){
+    console.log("10 REAIS")
+} else {
+    console.log("20 REAIS")
 }
