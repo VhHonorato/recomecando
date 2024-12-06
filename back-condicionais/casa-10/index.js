c7bbbacdec1565e2d9a7faa5108ffe7723a420d9
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 16:53:55 -0300

    Implementando a lógica para impressão do valor com base no tipo de pagamento

--- back-condicionais/casa-10/index.js
+++ back-condicionais/casa-10/index.js
@@ -1,13 +1,17 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "debito";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
-let valorDoProdutoNoCredito = valorDoProduto - (valorDoProduto * 0,05);
-let valorDoProdutoNoCheque = valorDoProduto - (valorDoProduto * 0,03);
-let valorDoProdutoNoDinOuDeb = valorDoProduto - (valorDoProduto * 0,10);
+let valorDoProdutoNoCredito = (valorDoProduto - (valorDoProduto * 0.05))/100; // valor em centavos
+let valorDoProdutoNoCheque = (valorDoProduto - (valorDoProduto * 0.03))/100; // valor em centavos
+let valorDoProdutoNoDinOuDeb = (valorDoProduto - (valorDoProduto * 0.10))/100; // valor em centavos
 
 if(tipoDePagamento === "credito"){
-    
+    console.log(`R$${valorDoProdutoNoCredito.toFixed(2)}`)
+} else if(tipoDePagamento === "cheque"){
+    console.log(`R$${valorDoProdutoNoCheque.toFixed(2)}`)
+} else if(tipoDePagamento === "debito" || "dinheiro"){
+    console.log(`R$${valorDoProdutoNoDinOuDeb.toFixed(2)}`)
 }
