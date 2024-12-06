39602a8324789ffd27712724b13b3cf30d67fd1e
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 19:28:40 -0300

    Implementada logica para impressão do nome, segundo os critérios estabelecidos.

--- back-condicionais/extra-12/index.js
+++ back-condicionais/extra-12/index.js
@@ -1,4 +1,13 @@
-const primeiroNome = "Mario";
-const sobrenome = "";
-const apelido = "";
+//Importante: Neste exercício, não deveremos utilizar nenhum operador de comparação, nem de igualdade e nem de identidade. Para este exercício deveremos utilizar apenas o conceito de truthiness.
 
+const primeiroNome = "Mario";
+const sobrenome = "Andrade";
+const apelido = "Bigode";
+
+if(!primeiroNome){
+    console.log("Preenchimento do primeiro nome é obrigatório")
+} else if(apelido){
+    console.log(apelido)
+} else if(primeiroNome){
+    console.log(sobrenome ? `${primeiroNome} ${sobrenome}` : primeiroNome)
+}
