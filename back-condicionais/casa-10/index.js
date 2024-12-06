1c5d244001c1d7cdf26f1ad34fdc87cfbf3747b0
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 16:37:42 -0300

    criando variaveis com os tipos de desconto.

--- back-condicionais/casa-10/index.js
+++ back-condicionais/casa-10/index.js
@@ -4,3 +4,10 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+let valorDoProdutoNoCredito = valorDoProduto - (valorDoProduto * 0,05);
+let valorDoProdutoNoCheque = valorDoProduto - (valorDoProduto * 0,03);
+let valorDoProdutoNoDinOuDeb = valorDoProduto - (valorDoProduto * 0,10);
+
+if(tipoDePagamento === "credito"){
+    
+}
