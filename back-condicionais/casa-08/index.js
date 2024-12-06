024f19f7043187d368a1f33b453fcc8cd4424f2e
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 11:03:56 -0300

    logica de idade testada - ok

--- back-condicionais/casa-08/index.js
+++ back-condicionais/casa-08/index.js
@@ -1,8 +1,10 @@
-const idade = 18;
+const idade = 10;
 const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
 if(idade < 12 || idade > 65){
     console.log("ACESSO NEGADO")
-}
+} else{
+    console.log("Acesso permitido")
+} 
