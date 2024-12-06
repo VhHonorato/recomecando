6ae5802590585c371b8636fe0d0aaf6bc4d726e5
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 17:09:44 -0300

    Implementada e testada logica para jogadaor 1 e jogador 2

--- back-condicionais/classe-01/index.js
+++ back-condicionais/classe-01/index.js
@@ -1,4 +1,20 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "tesoura"
+const jogada2 = "pedra"
 
 //seu código aqui
+
+if(jogada1 === jogada2){
+    console.log("EMPATE")
+} else if(jogada1 === "pedra" && jogada2 === "tesoura"){
+    console.log("Jogador 1 venceu")
+} else if(jogada1 === "papel" && jogada2 === "pedra"){
+    console.log("Jogador 1 venceu")
+} else if(jogada1 === "tesoura" && jogada2 === "papel"){
+    console.log("Jogador 1 venceu")
+}else if(jogada2 === "pedra" && jogada1 === "tesoura"){
+    console.log("Jogador 2 venceu")
+} else if(jogada2 === "papel" && jogada1 === "pedra"){
+    console.log("Jogador 2 venceu")
+} else if(jogada2 === "tesoura" && jogada1 === "papel"){
+    console.log("Jogador 2 venceu")
+}
