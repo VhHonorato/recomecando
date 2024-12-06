5958ec588a59eb44720882b001cc328fd4adaa70
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 12:32:26 -0300

    logica para imprimir os dias da semana usando if e else - intervalo de 1(segunda) a 7(domingo)

--- back-condicionais/casa-09/index.js
+++ back-condicionais/casa-09/index.js
@@ -1,1 +1,17 @@
-const diaDaSemana = 3;
+const diaDaSemana = 7;
+
+if(diaDaSemana === 1){
+    console.log("Segunda")
+} else if(diaDaSemana === 2){
+    console.log("Terça")
+} else if(diaDaSemana === 3){
+    console.log("Quarta")
+} else if(diaDaSemana === 4){
+    console.log("Quinta")
+} else if(diaDaSemana === 5){
+    console.log("Sexta")
+} else if(diaDaSemana === 6){
+    console.log("Sabado")
+} else if(diaDaSemana === 7){
+    console.log("Domingo")
+}    
