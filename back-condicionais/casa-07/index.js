9715ce1d691b4735bd7734f79ac31b988c8bb1f8
vhhonorato <vhhonorato@hotmail.com>
2024-12-06 10:53:22 -0300

    Resolução do exercício casa-07 com utilização de condicionais e operadores lógicos

--- back-condicionais/casa-07/index.js
+++ back-condicionais/casa-07/index.js
@@ -1,5 +1,14 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 2855971; //emCentavos
 
 //seu código aqui
+
+const rendimentoLimiteIsento = 2855970;
+ if(aposentada || portadoraDeDoenca){
+    console.log("ISENTA")
+ } else if(totalDeRendimentos <= rendimentoLimiteIsento){
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE")
+ } else {
+    console.log("PEGA LEAO")
+ }
