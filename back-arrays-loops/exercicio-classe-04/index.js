628ffbecd6973a48a0905d09f7388ecf3dfca0d6
vhhonorato <vhhonorato@hotmail.com>
2024-12-12 18:13:37 -0300

    Testando outros valores no array original.

--- back-arrays-loops/exercicio-classe-04/index.js
+++ back-arrays-loops/exercicio-classe-04/index.js
@@ -1,4 +1,4 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 4, 12, 21, 53, 88, 112, 6, 5, 14, 18, 3, 242, 77, 82];
 let numeroPar = [];
 for (let par of original) {
     if(par % 2 === 0) {
