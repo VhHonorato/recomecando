492e6bc2f5aeb5806d9f828661aa1a098395629a
vhhonorato <vhhonorato@hotmail.com>
2024-12-12 17:15:48 -0300

    resolução para verificar numero 10 no array - letra b - com correcão do código para o retorno de -1

--- back-arrays-loops/exercicio-classe-02/index.js
+++ back-arrays-loops/exercicio-classe-02/index.js
@@ -1,9 +1,14 @@
-const numeros = [54, 22, 14, 87, 284];
+const numeros = [54, 22, 14, 10, 284];
 
 for (let i = 0; i < numeros.length; i++) {
     if(numeros[i] === 10){
         console.log(i);
         break;
-    } 
+    } else if(i === numeros.length - 1){
+        console.log("-1");
+        break;
+    }
+    
 }
-console.log("-1");
+
+
