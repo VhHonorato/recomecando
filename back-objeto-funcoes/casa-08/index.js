927902cfe4086a46fbaff3dfce22cf067b14f294
vhhonorato <vhhonorato@hotmail.com>
2024-12-19 11:07:22 -0300

    c) Criado membro com a funcão desligar.

--- back-objeto-funcoes/casa-08/index.js
+++ back-objeto-funcoes/casa-08/index.js
@@ -1,15 +1,26 @@
 
 
 const carro = {
-    ligado: true,
-    velocidade: 0,
+    ligado: false,
+    velocidade: 10,
     ligar: function(){
         if(this.ligado){
           return  console.log("Este carro já está ligado")
         } else{
             this.ligado = true;
         }
+    },
+    desligar: function(){
+        if(!this.ligado){
+            return console.log("Este carro já está desligado");
+        } else {
+            this.ligado = false;
+            this.velocidade = 0;
+        }
     }
  
 }
-carro.ligar();
+// carro.ligar();
+carro.desligar();
+console.log(carro.ligado);
+console.log(carro.velocidade);
