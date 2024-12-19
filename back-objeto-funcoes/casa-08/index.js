75eb5552759815cf50165587dde06650628a62c8
vhhonorato <vhhonorato@hotmail.com>
2024-12-19 11:41:08 -0300

    f) Implementada mensagem em caso de alteração do valor de alguma propriedade.

--- back-objeto-funcoes/casa-08/index.js
+++ back-objeto-funcoes/casa-08/index.js
@@ -2,12 +2,13 @@
 
 const carro = {
     ligado: false,
-    velocidade: 10,
+    velocidade: 0,
     ligar: function(){
         if(this.ligado){
           return  console.log("Este carro já está ligado")
         } else{
             this.ligado = true;
+            return console.log(`Carro ligado. Velocidade: ${this.velocidade}`)
         }
     },
     desligar: function(){
@@ -16,6 +17,7 @@ const carro = {
         } else {
             this.ligado = false;
             this.velocidade = 0;
+            return console.log(`Carro desligado. Velocidade: ${this.velocidade}`)
         }
     },
     acelerar: function(){
@@ -23,20 +25,22 @@ const carro = {
             return console.log("Não é possivél acelerar um carro desligado")
         } else {
             this.velocidade += 10;
+            return console.log(`Carro ligado. Velocidade: ${this.velocidade}`)
         }
     },
     desacelerar: function(){
         if(!this.ligado){
             return console.log("Não é possível desacelerar um carro desligado")
         } else if(this.velocidade > 0){
-            this.velocidade -= 10
+            this.velocidade -= 10;
+            return console.log(`Carro ligado. Velocidade: ${this.velocidade}`)
         }
     }
  
 }
-// carro.ligar();
-// carro.desligar();
-// carro.acelerar();
+carro.ligar();
+carro.acelerar();
 carro.desacelerar();
+carro.desligar();
 console.log(carro.ligado);
 console.log(carro.velocidade);
