29c837a339cf43efa298e3d5fceca4493eb7dad9
vhhonorato <vhhonorato@hotmail.com>
2024-12-19 10:09:27 -0300

    Criada função para correcão de prova.

---
+++ back-objeto-funcoes/casa-07/index.js
@@ -0,0 +1,40 @@
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+};
+
+function corrigirProva(prova) {
+    let nota = 0;
+    let questao = 0;
+    for(let nota of prova.questoes) {
+        if(nota.resposta == nota.correta) {
+           nota += 2; 
+           questao += 1;
+        }
+    }
+    console.log(`O aluno(a) ${prova.aluno} acertou ${questao} questões e obteve nota ${nota}.`)
+}
+corrigirProva(prova);
