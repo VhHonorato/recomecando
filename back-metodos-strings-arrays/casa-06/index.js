6ace9d9664e62b76b8b681509ed18cd3a620a219
vhhonorato <vhhonorato@hotmail.com>
2025-01-13 17:56:34 -0300

    Implementada mensagem em caso de numero de CPF inválido

--- back-metodos-strings-arrays/casa-06/index.js
+++ back-metodos-strings-arrays/casa-06/index.js
@@ -2,9 +2,12 @@ const cpf = "12345678900";
 const cnpj = "12345678900";
 
 function formatarCpf (nCpf) {
+    if(nCpf.length != 11){
+        console.log("CPF inválido")
+    } else {    
     nCpf = nCpf.toString();
     return console.log(nCpf.slice(0,3) + "." + nCpf.slice(3,6) + "." + nCpf.slice(6,9) + "-" + nCpf.slice(9));
-
+}
 }
 
 
