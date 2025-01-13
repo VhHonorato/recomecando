edfa03ece00e69467dddacaedac256f1739cadb1
vhhonorato <vhhonorato@hotmail.com>
2025-01-13 15:56:22 -0300

    Implementada função para bloquear palavaras proibidas, com retorno de mensagem.

--- back-metodos-strings-arrays/casa-05/index.js
+++ back-metodos-strings-arrays/casa-05/index.js
@@ -1,1 +1,13 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Essa PANDEMIA é muito perigosa!";
+let palavrasProibidas = ["covid", "pandemia"];
+function bloquearConteudo(texto) {
+    for(let palavra of palavrasProibidas) {
+    if (texto.toLowerCase().includes(palavra)){
+     return console.log("Comentário bloqueado por conter palavras proibidas!")
+
+    } 
+
+}
+    console.log(texto);
+}
+bloquearConteudo(comentario);
