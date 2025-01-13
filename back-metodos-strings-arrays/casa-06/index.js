4078b55f4b38639e9b1cec14493fb4395a783ece
vhhonorato <vhhonorato@hotmail.com>
2025-01-13 17:52:10 -0300

    Implementacão de funcão para formatarcão de numero de CPF

--- back-metodos-strings-arrays/casa-06/index.js
+++ back-metodos-strings-arrays/casa-06/index.js
@@ -1,2 +1,11 @@
 const cpf = "12345678900";
 const cnpj = "12345678900";
+
+function formatarCpf (nCpf) {
+    nCpf = nCpf.toString();
+    return console.log(nCpf.slice(0,3) + "." + nCpf.slice(3,6) + "." + nCpf.slice(6,9) + "-" + nCpf.slice(9));
+
+}
+
+
+formatarCpf(cpf);
