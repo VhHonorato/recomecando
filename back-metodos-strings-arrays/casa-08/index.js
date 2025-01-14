ba89e4aa1aae4f0f130e1f0e8889e758be7548fd
vhhonorato <vhhonorato@hotmail.com>
2025-01-14 17:15:59 -0300

    Implementada função para formatação do nome.

--- back-metodos-strings-arrays/casa-08/index.js
+++ back-metodos-strings-arrays/casa-08/index.js
@@ -2,3 +2,24 @@ let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
 let tags = ['financeiro', 'administrativo', 'geral'];
+
+function formatIdentificador (identificador) {
+    console.log(identificador.padStart(6,"0"));
+}
+
+formatIdentificador(identificador);
+
+ function formatNome (nome) {
+      let arrayNome = nome.split(" ");
+      let nomeFormat = "";
+      for(let i = 0; i < arrayNome.length; i++) {
+        let nomes = arrayNome[i];
+        let nomesArray = nomes.split("")
+        let letraM = nomesArray[0].toUpperCase() 
+        nomesArray.splice(0,1,`${letraM}`);
+        nomeFormat += `${nomesArray.join("")} `;      
+      }
+      console.log(nomeFormat);
+}
+
+formatNome(nome);
