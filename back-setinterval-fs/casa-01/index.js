ca92545f2c8050aec84551f3715977c23d109876
vhhonorato <vhhonorato@hotmail.com>
2025-01-15 20:30:43 -0300

    Implementado temporizador de jogada com setInterval.

--- back-setinterval-fs/casa-01/index.js
+++ back-setinterval-fs/casa-01/index.js
@@ -1,1 +1,16 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+let indice = 0;
+function jogada (jogadores){
+    if(indice == jogadores.length){
+        console.log("A rodada terminou!")
+        clearInterval(intervalId);
+    } else {
+        console.log(`${jogadores[indice]}`);
+        indice++
+        }
+};
+jogada(jogadores);
+const intervalId = setInterval(jogada, 2000, jogadores);
+
+
+
