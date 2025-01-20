f1c70893bf859d11b96890822c10794983a847ea
vhhonorato <vhhonorato@hotmail.com>
2025-01-20 17:14:12 -0300

    Filtradas cicades, do array, com até 8 caractéres.

--- back-metodos-funcionais/casa-05/index.js
+++ back-metodos-funcionais/casa-05/index.js
@@ -1,1 +1,3 @@
-const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro', 'Aracaju'];
+const oitoCaracteres = cidades.filter(x => x.length <= 8);
+console.log(oitoCaracteres.join(", "));
