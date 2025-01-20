e4d522657f4210a235e4a8c29c3b12caa60042b3
vhhonorato <vhhonorato@hotmail.com>
2025-01-20 19:16:07 -0300

    Criado novo array com a formatacão requerida.

--- back-metodos-funcionais/casa-07/index.js
+++ back-metodos-funcionais/casa-07/index.js
@@ -1,1 +1,7 @@
-const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+const frutas = ["manga", "UVA", "abacaxi", "banaNA", "MAçã", "cAjArANA"];
+const novoArray = frutas.map((x, i) => {
+    const letraMinuscula = x.toLowerCase();
+    const letraMaiuscula = letraMinuscula[0].toUpperCase() + letraMinuscula.slice(1);
+    return `${i} - ${letraMaiuscula}` 
+});
+console.log(novoArray);
