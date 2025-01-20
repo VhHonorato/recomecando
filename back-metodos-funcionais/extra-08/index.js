8dc605145e7b25da91e4d410d454fe388db38b2d
vhhonorato <vhhonorato@hotmail.com>
2025-01-20 19:21:29 -0300

    Implementado filtro  de numeors inteiros positivos, no array.

--- back-metodos-funcionais/extra-08/index.js
+++ back-metodos-funcionais/extra-08/index.js
@@ -1,1 +1,4 @@
-const numeros = [10, 987, -886, 0, 12, 199, -45, -67];
+const numeros = [10, 987, -886, 0, 12, 199, -45, -67, 19, -76];
+
+const numerosPositivos = numeros.filter(x => x > 0);
+console.log(numerosPositivos);
