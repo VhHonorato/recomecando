5bf8c5b3e83c082a06a31527a142e18d1cf2a1fa
vhhonorato <vhhonorato@hotmail.com>
2025-01-23 17:39:27 -0300

    Criado servidor para controlar rodadas de um jogo.

--- back-primeiro-servidor/classe-03/index.js
+++ back-primeiro-servidor/classe-03/index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const app = express();
+
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+let indice = 0;
+app.get("/jogada", (req,res) => {
+    console.log("jogada realizada");
+   if(indice == jogadores.length){
+    indice = 0;
+    res.send(`É a vez de ${jogadores[indice]} jogar`);
+    indice++ 
+   } else {
+    res.send(`É a vez de ${jogadores[indice]} jogar`);
+    indice++ 
+   }
+    console.log(indice);
+})
+
+
+
+app.listen(8000)
