9f9807d9798203d8dcb645b8ea2e1260685a0213
vhhonorato <vhhonorato@hotmail.com>
2025-01-24 12:42:52 -0300

    Criada rota com a logica para pausar o cronometro.

--- back-primeiro-servidor/casa-06/index.js
+++ back-primeiro-servidor/casa-06/index.js
@@ -4,15 +4,15 @@ const app = express();
 //Primeira rota - recurso raiz
 let segundos = 0;
 let minutos = 0;
-// function cronometro(minutos, segundos) {
-   setInterval(() => { 
-        segundos++;  
+// function cronometro(minutos, segundos) 
+ function cronometro () {
+    segundos++;  
         if(segundos == 60){
             minutos+=1;
             segundos = 0;
         } 
-        
-    }, 1000);
+ }
+ let cronometroId = 0;
   
     console.log(`Tempo atual do cronômetro: ${minutos} minutos e ${segundos} segundos`)
 
@@ -21,9 +21,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/iniciar", (req, res) => {
-    res.send(`Tempo atual do cronômetro: ${minutos} minutos e ${segundos} segundos`);
-    
+    cronometroId = setInterval( cronometro, 1000);
+  
+    res.send("Cronometro iniciado")
+});
+app.get("/pausar", (req, res) => {
+    clearInterval(cronometroId);
+    res.send("Cronometro pausado")
 })
+
 
 
 app.listen(8000);
