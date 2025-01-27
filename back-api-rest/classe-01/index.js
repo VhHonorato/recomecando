02176f5118a79f2f5fb05d4117dac0f1c10515f3
vhhonorato <vhhonorato@hotmail.com>
2025-01-27 12:31:14 -0300

    Implementada rota get para consultar existencia de nomes na lista de convidados.

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -10,6 +10,26 @@ const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto
 
 app.get('/convidados', (req, res) => {
     res.json(convidados);
-})
+});
+ //b) Consulta se existe um nome específico na lista de convidados
+
+ app.get('/convidados/:nome', (req, res) => {
+    const convidado = convidados.find((convidado) => convidado === req.params.nome);
+    const lista = {
+        mensagem: ""
+    }
+    console.log(convidado);
+    if(req.params.nome !== undefined){
+        if(!convidado){
+            res.json(lista.mensagem = "O convidado buscado não está na lista.");
+        }else{
+            res.json(lista.mensagem = "O convidado presente.")
+        }
+    } else{
+        res.json(convidados);
+    }
+});
+
+
 
 app.listen(8000);
