722593f1d6ae22107994aa97b7fb4ce5e8324050
vhhonorato <vhhonorato@hotmail.com>
2025-01-27 11:36:11 -0300

    Criada rota para consultar lista decompleta de convidados.

--- back-api-rest/classe-01/index.js
+++ back-api-rest/classe-01/index.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 
+const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
 
+// a) Consulta a lista inteira de convidados
+
+app.get('/convidados', (req, res) => {
+    res.json(convidados);
+})
 
 app.listen(8000);
