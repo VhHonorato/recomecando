5a1df02527fb3cb3a86ccad7434ae046b0452dfd
vhhonorato <vhhonorato@hotmail.com>
2025-02-04 17:29:30 -0300

    Implementada rota POST para inserção de um novo aluno na colecão de alunos. Com verificacões para todos os itens do body.

--- back-rotas-controladores/extra-02/controladores/alunos.js
+++ back-rotas-controladores/extra-02/controladores/alunos.js
@@ -1,6 +1,5 @@
 const alunos = require("../dados/alunos");
 
-
 function consultarAlunos(req, res) {
     res.status(200);
     res.json(alunos);
@@ -23,7 +22,79 @@ function consultarAlunosId(req, res) {
     }
 };
 
+let proximoId = 1; 
+
+function cadastrarAluno(req, res) {
+    
+    if(!req.body.nome){
+        res.status(400);
+        res.json({message: "O campo 'nome' é obrigatório."});
+        return;
+    };
+    if(!req.body.sobrenome){
+        res.status(400);
+        res.json({message: "O campo 'sobrenome'é obrigatório."});
+        return;
+    };
+    if(!req.body.curso){
+        res.status(400);
+        res.json({message: "O campo 'curso' é obrigatório."});
+        return;
+    };
+    if(!req.body.idade){
+        res.status(400);
+        res.json({message: "O campo 'idade' é obrigatório."});
+        return;
+    };
+
+    if(req.body.nome.trim().length < 1 ){
+        res.status(400);
+        res.json({message: "O campo nome deve ser preenchido com um nome válido."});
+        return;
+    };
+    if(req.body.sobrenome.trim().length < 1 ){
+        res.status(400);
+        res.json({message: "O campo nome deve ser preenchido com um sobrenome válido."});
+        return;
+    };
+    if(Number(req.body.idade) < 18){
+        res.status(400);
+        res.json({message: "O aluno deve ter idade maior que 18 anos."});
+        return;
+
+    };
+    if(typeof req.body.nome !== "string"){
+        res.status(400);
+        res.json({message: "O nome deve ser um texto"});
+        return;
+    };
+    if(typeof req.body.sobrenome !== "string"){
+        res.status(400);
+        res.json({message: "O sobrenome deve ser um texto"});
+        return;
+    };
+    if(typeof req.body.curso !== "string"){
+        res.status(400);
+        res.json({message: "O curso deve ser um texto"});
+        return;
+    };
+    const novoAluno = {
+        id: proximoId,
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        idade: req.body.idade,
+        curso: req.body.curso
+    };
+    alunos.push(novoAluno);
+    console.log(alunos);
+    proximoId += 1;
+    res.status(201);
+    res.json({message: "Aluno adicionado."})
+};
+
 module.exports = {
     consultarAlunos,
-    consultarAlunosId
+    consultarAlunosId,
+    cadastrarAluno,
+    
 };
--- back-rotas-controladores/extra-02/roteador.js
+++ back-rotas-controladores/extra-02/roteador.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {consultarAlunos, consultarAlunosId} = require('./controladores/alunos');
+const {consultarAlunos, consultarAlunosId,cadastrarAluno} = require('./controladores/alunos');
 const roteador = express();
 
 roteador.get("/alunos", consultarAlunos);
 roteador.get("/alunos/:id", consultarAlunosId);
+roteador.post("/alunos", cadastrarAluno);
 
 module.exports = roteador;
